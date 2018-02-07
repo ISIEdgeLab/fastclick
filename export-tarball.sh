fdda1e7eaaff11bc23433efae0ea17164a3464be
Robert Story <rstory@localhost>
2018-02-07 04:33:59 -0800

    update to export-tarball script

--- export-tarball.sh
+++ export-tarball.sh
@@ -3,11 +3,23 @@
 # bail on errors
 set -e
 
+base=$(basename $PWD)
 dest="/proj/edgect/share/dpdk/tarballs"
 
-while getopts d: opt
+usage()
+{
+   echo "$0 [-b basename] [-d dir] [hash|tag]"
+   echo "  -b basename: base name for tarball [default: $base]"
+   echo "  -d dir: export tarball to dir [default: $dest]"
+   exit 1
+}
+
+[ ! -d .git ] && echo "you must run $0 for the top level of the git repository" && exit 1
+
+while getopts b:d: opt
 do
     case "$opt" in
+      b)  base=$OPTARG;;
       d)  dest=$OPTARG;;
       \?) usage;;
     esac
@@ -23,8 +35,14 @@ hash="$1"
 git log -1 $hash 1>/dev/null 2>/dev/null && rc=0 || rc=1
 [ $rc -eq 1 ] && echo "bad hash $hash" && exit 1
 
-echo "* Exporting tarball for commit $hash"
-git archive $hash -o $dest/edgelab-elements-git.${hash}.tar.gz
+echo -n "* Export $base-$hash.tar.gz?"
+read ans
+case $ans in
+  Y|y|YES|yes) ;;
+  *) echo "tarball not exported"
+     exit 1;;
+esac
+git archive $hash -o $dest/$base-${hash}.tar.gz --prefix=$base/
 
-echo "* Created $dest/edgelab-elements-git.${hash}.tar.gz"
-echo "* Add EDGELABELEMENTS_VERSION='git.${hash}' to your dpdk-version.inc to use"
+echo "* Created $dest/$base-${hash}.tar.gz"
+#echo "* Add FASTCLICK_VERSION='git.${hash}' to your edge-vars.inc to use"
